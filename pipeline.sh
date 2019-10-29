8a61c072a2a1d59b831139c6bde5e4a79de6082d
rmuhamed <ricardo.muhamed@gmail.com>
2019-10-29 21:43:09 +0100

    Pipline improvements

--- pipeline.sh
+++ pipeline.sh
@@ -5,7 +5,8 @@ go get github.com/labstack/echo
 go get github.com/gorilla/websocket
 
 # cleanup
-rm -rf build
+rm -rf build || true
+pkill votingapp
 
 # build
 mkdir build
@@ -13,5 +14,33 @@ mkdir build
 go build -o ./build ./src/votingapp
 cp -r ./src/votingapp/ui ./build
 
-set +e
+pushd build 
+./votingapp &
+popd
 
+# test
+curl --url http://localhost:2000/vote \
+    --request POST \
+    --data '{"topics":["dev", "ops"]}' \
+    --header "Content-Type: application/json"
+
+curl --url http://localhost:2000/vote \
+    --request PUT \
+    --data '{"topic": "dev"}' \
+    --header "Content-Type: application/json"
+
+winner=$(curl --url http://localhost:2000/vote \
+    --request DELETE \
+    --header "Content-Type: application/json" | ./jq -r '.winner') 
+
+echo "Winner IS " $winner
+
+expectedWinner = "dev"
+
+if [ "$expectedWinner" == "$winner"]; then
+    echo 'TEST PASSED'
+    exit 0
+else 
+    echo 'TEST FAILED'
+    exit 1
+fi
