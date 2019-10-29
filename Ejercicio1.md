2b90b58cb936c7c01afb84b59bdf6832c8eeddb5
paulopez78 <pau.lopez.78@gmail.com>
2019-10-29 21:58:08 +0100

    siguiente clase

---
+++ Ejercicio1.md
@@ -0,0 +1,7 @@
+# Ejercicio 1: Scripting Pipelines
+
+Reescribir los **tests de integración** realizados en clase con bash, pero esta vez usando `python` como lenguaje de scripting, añadir **política de reintentos** al ejecutar los test.
+
+* Opcional: Reescribir los tests usando `nodejs` o cualquier otro lenguaje de programación.
+
+La entrega se realiza en un repositorio privado de **github** de cada estudiante, enviando invitación a **paulopez78@gmail.com**
--- pipeline.sh
+++ pipeline.sh
@@ -2,13 +2,75 @@
 set -e
 
 # install deps
-go get github.com/gorilla/websocket
-go get github.com/labstack/echo
+deps(){
+    go get github.com/gorilla/websocket
+    go get github.com/labstack/echo
+}
 
 # cleanup
-rm -rf build
+cleanup(){
+    rm -rf build || true
+    pkill votingapp || ps aux | grep votingapp | awk {'print $2'} | head -1 | xargs kill -9
+}
 
 # build 
-mkdir build
-go build -o ./build ./src/votingapp 
-cp -r ./src/votingapp/ui ./build
+build(){
+    mkdir build
+    go build -o ./build ./src/votingapp 
+    cp -r ./src/votingapp/ui ./build
+
+    pushd build
+    ./votingapp &
+    popd
+}
+
+retry(){
+    n=0
+    interval=5
+    retries=3
+    $@ && return 0
+    until [ $n -ge $retries ]
+    do
+        n=$[$n+1]
+        echo "Retrying...$n of $retries, wait for $interval seconds"
+        sleep $interval
+        $@ && return 0
+    done
+
+    return 1
+}
+
+# test
+test() {
+    votingurl='http://localhost/vote'
+    curl --url  $votingurl \
+        --request POST \
+        --data '{"topics":["dev", "ops"]}' \
+        --header "Content-Type: application/json" 
+
+    curl --url $votingurl \
+        --request PUT \
+        --data '{"topic": "dev"}' \
+        --header "Content-Type: application/json" 
+    
+    winner=$(curl --url $votingurl \
+        --request DELETE \
+        --header "Content-Type: application/json" | jq -r '.winner')
+
+    echo "Winner IS "$winner
+
+    expectedWinner="dev"
+
+    if [ "$expectedWinner" == "$winner" ]; then
+        echo 'TEST PASSED'
+        return 0
+    else
+        echo 'TEST FAILED'
+        return 1
+    fi
+}
+
+deps
+cleanup
+build
+retry test
