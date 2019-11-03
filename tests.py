dfca56ab229bf7f27d25b752952afa5a128252a8
rmuhamed <ricardo.muhamed@gmail.com>
2019-11-03 10:47:58 +0100

    Added retry policy to tests script

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from retrying import retry
 
 def doPost(votingappUrl, payload):
     response = requests.post(url = votingappUrl, data = payload)
@@ -11,8 +12,12 @@ def doDelete(votingappUrl):
     response = requests.delete(votingappUrl)
     return response.json()
 
-url='http://localhost:8080/vote'
+@retry(stop_max_attempt_number=3, wait_fixed=5000)
+def doRequests():
+    url='http://localhost:8080/vote'
+    doPost(url, {'topics': ['dev', 'ops']})
+    doPut(url, {'topic': 'dev'})
+    doDelete(url)
 
-doPost(url, {'topics': ['dev', 'ops']})
-doPut(url, {'topic': 'dev'})
-doDelete(url)
+
+doRequests()
