663224fe65f06001657b370516664f838a26d9b9
rmuhamed <ricardo.muhamed@gmail.com>
2019-11-05 21:25:48 +0100

    Docker enhancements

--- pipeline.sh
+++ pipeline.sh
@@ -1,78 +1,30 @@
 #!/bin/bash
 set -e
 
-# install deps
-deps(){
-    go get github.com/gorilla/websocket
-    go get github.com/labstack/echo
-}
+docker network create votingapp || true
 
 # cleanup
-cleanup(){
-    docker rm -f myvotingapp
-    rm -rf build || true
-}
+docker rm -f myvotingapp || true
 
 # build
-build(){
-    mkdir build
-    go build -o ./build ./src/votingapp
-    cp -r ./src/votingapp/ui ./build
+docker build \
+-t rmuhamed/votingapp \
+./src/votingapp
 
-    docker build -f src/votingapp/Dockerfile -t rmuhamed/votingapp .
-    docker run --name myvotingapp -p 8080:80 -d rmuhamed/votingapp
-}
-
-retry(){
-    n=0
-    interval=5
-    retries=3
-    $@ && return 0
-    until [ $n -ge $retries ]
-    do
-        n=$[$n+1]
-        echo "Retrying...$n of $retries, wait for $interval seconds"
-        sleep $interval
-        $@ && return 0
-    done
-
-    return 1
-}
+docker run \
+    --name myvotingapp \
+    --network votingapp
+    -p 8080:80 \
+    -d rmuhamed/votingapp
 
 # test
-test() {
-    votingurl='http://localhost:8080/vote'
-    curl --url  votingurl \
-        --request POST \
-        --data '{"topics":["dev", "ops"]}' \
-        --header "Content-Type: application/json"
-
-    curl --url $votingurl \
-        --request PUT \
-        --data '{"topic": "dev"}' \
-        --header "Content-Type: application/json"
-
-    winner=$(curl --url $votingurl \
-        --request DELETE \
-        --header "Content-Type: application/json" | jq -r '.winner')
-
-    echo "Winner IS "$winner
-
-    expectedWinner="dev"
-
-    if [ "$expectedWinner" == "$winner" ]; then
-        echo 'TEST PASSED'
-        return 0
-    else
-        echo 'TEST FAILED'
-        return 1
-    fi
-}
-
-deps
-cleanup || true
-GOOS=linux build
-retry test
+docker build \
+-t rmuhamed/votingapp-test \
+./test
+docker run \
+    -rm -e VOTINGAPP_HOST="myvotingapp" \
+    --network votingapp \
+    rmuhamed/votingapp-test
 
 #delivery
 docker push rmuhamed/votingapp
