8423be3978087cdb89e7d06408c3148f54cb2aa4
rmuhamed <ricardo.muhamed@gmail.com>
2019-11-12 21:41:15 +0100

    Integration tests added to pipeline

--- pipeline-k8s.sh
+++ pipeline-k8s.sh
@@ -4,7 +4,11 @@ docker-compose rm -f && \
 docker-compose up --build -d && \
 docker-compose run --rm mytest && \
 docker-compose push && \
+kubectl apply -f votingapp.yaml && \
+kubectl run mytests \
+    --generator=run-pod/v1 \
+    --image=rmuhamed/votingapp-test \
+    --env VOTINGAPP_HOST=myvotingapp.votingapp \
+    --rm --attach --restart=Never
 echo "GREEN" || echo "RED"
 
-#deploy
-kubectl apply -f votingapp.yaml
