4c41352ae34ee8b2f3869a689324328d5ed31796
rmuhamed <ricardo.muhamed@gmail.com>
2019-11-18 20:24:45 +0100

    Docker run fixed indentation

--- pipeline.sh
+++ pipeline.sh
@@ -12,19 +12,20 @@ docker build \
 ./src/votingapp
 
 docker run \
-    --name myvotingapp \
-    --network votingapp
-    -p 8080:80 \
-    -d rmuhamed/votingapp
+--name myvotingapp \
+--network votingapp \
+-p 8080:80 \
+-d rmuhamed/votingapp
 
 # test
 docker build \
 -t rmuhamed/votingapp-test \
 ./test
+
 docker run \
-    -rm -e VOTINGAPP_HOST="myvotingapp" \
-    --network votingapp \
-    rmuhamed/votingapp-test
+--rm -e VOTINGAPP_HOST="myvotingapp" \
+--network votingapp \
+rmuhamed/votingapp-test
 
 #delivery
 docker push rmuhamed/votingapp
